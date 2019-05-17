6f9c6a71c49429ff9fe8a2984ce122efb5cc6a22
DomRowney <dominic.rowney@nhs.net>
2019-05-17 11:22:03 +0100

    Added existing animate code, with ordinal scale for time durations
    Signed-off-by: DomRowney <dominic.rowney@nhs.net>

---
+++ ProcessAnimateR_vignette.R
@@ -0,0 +1,35 @@
+# Libraries ---------------------------------------------------------------
+
+library(dplyr) ##pipes
+library(tidyr) ##tidy data
+library(lubridate) ##date time
+library(bupaR) ##buisness process analytics
+library(processanimateR) ##animates process
+
+
+# Create performance times ------------------------------------------------
+
+my_flags <- data.frame(value = c(0,2,4,8,16)) %>% 
+  mutate(day = days(value))
+
+
+my_timeflags <- patients %>% 
+  cases %>%
+  crossing(my_flags) %>% 
+  mutate(time = start_timestamp + day) %>% 
+  filter(time <= complete_timestamp) %>% 
+  select("case" = patient,time,value) ##must be case, time, value
+
+
+# Animate process ---------------------------------------------------------
+
+patients %>%
+  animate_process(mode ="absolute",
+                  jitter=10,
+                  legend = "color", 
+                  mapping = token_aes(
+                    color = token_scale(my_timeflags
+                                        , scale = "ordinal"
+                                        , domain = my_flags$value
+                                        , range = rev(RColorBrewer::brewer.pal(5,"Spectral"))
+                    )))
